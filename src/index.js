c2129301662a6ea339557cde34cfd2e5a86c1786
Na-seunghyun <looloo90@naver.com>
2026-06-17 08:47:01 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-const ALLOWED_ASSETS = new Set([
-  "/troop_training_ui.html",
-  "/kingshot_calculator_data.json",
-]);
-
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
     if (url.pathname === "/") {
-      url.pathname = "/troop_training_ui.html";
-      return env.ASSETS.fetch(new Request(url, request));
+      const pageUrl = new URL("/troop_training_ui.html", url.origin);
+      return env.ASSETS.fetch(new Request(pageUrl, request));
     }
 
-    if (ALLOWED_ASSETS.has(url.pathname)) {
-      return env.ASSETS.fetch(request);
-    }
-
-    return new Response("Not found", { status: 404 });
+    return env.ASSETS.fetch(request);
   },
 };
